1607486417f3e28eb192c5097c0d4cece8879e17
Santos Alberto Ortiz Chavez <sortiz@internal.inss.org>
2025-01-09 17:10:10 -0600

    Process the extracted data into a spreeedsheet

--- api_scraper/remoteok_scraper.py
+++ api_scraper/remoteok_scraper.py
@@ -19,6 +19,29 @@ def get_job_postings():
     res = requests.get(url=BASE_URL, headers=REQUEST_HEADER)
     return res.json()
 
+def outputs_job_to_xls(data):
+    wb = Workbook()
+    job_sheet = wb.add_sheet('Jobs')
+    headers = list(data[0].keys())
+    print(headers)
+
+    # write the headers into the spreedsheet
+    for i in range(0, len(headers)):
+        job_sheet.write(0, i, headers[i])
+    
+    # loop through the jobs data to write into a spreedsheet
+    for i in range(0, len(data)):
+        job = data[i]
+        values = list(job.values())
+        
+    #   loop through the elements to write a record columns
+        for j in range(0, len(values)):
+            job_sheet.write(i+1, j, values[j])
+
+    # save the spreetshet
+    wb.save('remote_jobs.xls')
+
 if __name__ == "__main__":
-    json = get_job_postings()[1]
+    json = get_job_postings()[1:]
     print(json)
+    outputs_job_to_xls(json)
